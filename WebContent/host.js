ac651a0dabf72c0898daff1deaade4089d45d666
jack-zdl <zhangdelei@bsgchina.com>
2017-05-15 21:33:57 +0800

    添加cluster network 部分代码

--- WebContent/host.js
+++ WebContent/host.js
@@ -4,8 +4,9 @@ var host="http://localhost:8080"
 //var host="http://192.168.2.247:8080";
 var adminLoginName = "";
 var siteId = "79affb2429ef48faa57c802a88dd8e7f";
-var isSetJqgrid = 0;
 var FreshenTime = 5e3;
+
+//要求每个任务页面都要有刷新，而且每个刷新时间都不一样
 var isTimer = null;
 
 
