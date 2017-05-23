2ed92a187b6b94ad99c461dfbd7ee31cad14f0e5
jack-zdl <zhangdelei@bsgchina.com>
2017-05-23 08:15:21 +0800

    添加站点

--- WebContent/host.js
+++ WebContent/host.js
@@ -1,12 +1,17 @@
-// JavaScript Documen 192.168.100.136
+//upm_api主机ip
 //var host="http://192.168.2.189:8080";
 var host="http://localhost:8080"
-//var host="http://192.168.2.247:8080";
+//var host="http://192.168.2.248:8080";
+
+
+
+
+
+//要求每个任务页面都要有刷新，而且每个刷新时间都不一样
 var adminLoginName = "";
 var siteId = "79affb2429ef48faa57c802a88dd8e7f";
 var FreshenTime = 5e3;
 
-//要求每个任务页面都要有刷新，而且每个刷新时间都不一样
-var isTimer = null;
+
 
 
